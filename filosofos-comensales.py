a5d51d752a880a86087ad38b4da6c3d6c8cd9a13
Oscar Quintero <oscar24quintero@hotmail.com>
2021-11-07 12:13:08 -0600

    Título de la actividad

--- filosofos-comensales.py
+++ filosofos-comensales.py
@@ -1,1 +1,19 @@
 # -*- coding: utf_8 -*-
+import os
+
+
+def limpiarPantalla():	
+	if(os.name == "posix"):
+		os.system("clear")
+	elif(os.name == "ct" or os.name == "nt" or os.name == "dos"):
+		os.system("cls")
+
+#---------------------------------------------------------------------------
+limpiarPantalla()
+print("\n")
+print("Actividad 12 - Filósofos Comensales")
+print("\n")
+print("Seminario de Sistemas Operativos - 2021 B")
+print("Oscar Alejandro Quintero Iñiguez")
+print("\n")
+
