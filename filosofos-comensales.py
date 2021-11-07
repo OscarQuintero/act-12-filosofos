7b47525faf08dc33dd718203ea68872ae7a7a044
Oscar Quintero <oscar24quintero@hotmail.com>
2021-11-07 12:41:14 -0600

    Vista de los Filósofos y Palillos

--- filosofos-comensales.py
+++ filosofos-comensales.py
@@ -1,12 +1,17 @@
 # -*- coding: utf_8 -*-
 import os
+import time
 
+#Retardo en Segundos para las pasusas:
+RETARDO = 3 
 
 def limpiarPantalla():	
 	if(os.name == "posix"):
 		os.system("clear")
 	elif(os.name == "ct" or os.name == "nt" or os.name == "dos"):
 		os.system("cls")
+
+
 
 #---------------------------------------------------------------------------
 limpiarPantalla()
@@ -16,4 +21,22 @@ print("\n")
 print("Seminario de Sistemas Operativos - 2021 B")
 print("Oscar Alejandro Quintero Iñiguez")
 print("\n")
+time.sleep(0)
+
+print("\tPalillo 5: Ocupado por...\n")
+print("\u0332".join("Filósofo 1: "), "Esperando...\n")
+print("\tPalillo 1: Ocupado por...\n")
+print("\u0332".join("Filósofo 2: "), "Esperando...\n")
+print("\tPalillo 2: Ocupado por...\n")
+print("\u0332".join("Filósofo 3: "), "Esperando...\n")
+print("\tPalillo 3: Ocupado por...\n")
+print("\u0332".join("Filósofo 4: "), "Esperando...\n")
+print("\tPalillo 4: Ocupado por...\n")
+print("\u0332".join("Filósofo 5: "), "Esperando...\n")
+print("\tPalillo 5: Ocupado por...\n")
+
+
+
+
+
 
